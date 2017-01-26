d6a0ddd637ec13dc9a582b0c9eb1feb380161a19
Dmitri Akatov <akatov@gmail.com>
2017-01-26 14:16:13 +0000

    delete all sourcemap files in distDir

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
 /* jshint node: true */
 'use strict';
 
+var Promise = require('ember-cli/lib/ext/promise');
+var fs = require('fs');
+var path = require('path');
+
 module.exports = {
-  name: 'ember-cli-deploy-delete-sourcemaps'
+  name: 'ember-cli-deploy-delete-sourcemaps',
+
+  createDeployPlugin: function(options) {
+    return {
+      name: options.name,
+
+      didBuild: function(context) {
+        var promises = context.distFiles
+            .filter(function(file) {
+              return file.match(/^assets\/.*\.map$/)
+            })
+            .map(function(file) {
+              return path.join(context.distDir, file);
+            })
+            .map(function(file) {
+              return new Promise((resolve, reject) => {
+                fs.unlink(file, function(err) {
+                  if (err) {
+                    reject();
+                  } else {
+                    resolve();
+                  }
+                });
+              })});
+        return Promise.all(promises);
+      }
+    };
+  }
 };
